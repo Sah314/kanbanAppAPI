47c1cd42d18899aad764fb1fa9303b10b492faf8
sah_314 <khadayatesahil@gmail.com>
2023-09-21 00:38:47 +0530

    Updated token validity checks

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -4,16 +4,18 @@ require("dotenv").config('.env');
 
 const authenticate = async(req,res,next) => {
 const authHeader = req.headers['authorization']
-console.log("header: ",authHeader);
 const token = authHeader && authHeader.split(' ')[1];
 if(token==null){
     return res.status(404).json({"message":"Token not found"})
 }
 const usertoken = await user.findOne({jwtToken:token});
-const validity = usertoken.jwtExpiresAt;
-if(validity<=Date.now()){
-    return res.status(401).json({"message":"token validity reached, please login again..."});
+// console.log(usertoken);
+const validity = new Date(usertoken.jwtExpiresAt);
+
+if (validity <= new Date()) {
+  return res.status(401).json({ "message": "Token validity reached, please login again..." });
 }
+
 jwt.verify(token,process.env.jwtSecret,(err,user)=>{
     if(err) return res.status(403).json({"message":"Invalid token"});
     req.user=user;
